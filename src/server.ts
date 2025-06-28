8748ccd265dee251a2fba1c269d6b95b38a1175e
GNaves <gabrielnaves60@gmail.com>
2025-06-28 01:02:15 -0300

    add on git

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import fastify from "fastify";
+import { knex } from "./database";
+
+const app = fastify();
+
+app.get("/hello", async () => {
+  const test = await knex("sqlite_schema").select("*");
+
+  return test;
+});
+
+app.listen({ port: 3333 }).then(() => {
+  console.log("server is running on port 3333");
+});
