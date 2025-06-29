199ddb997328f69c65c2b7f5cfb7bc8a0b9115d0
GNaves <gabrielnaves60@gmail.com>
2025-06-29 01:18:47 -0300

    feat: new tests

--- src/tests/transactions.spec.ts
+++ src/tests/transactions.spec.ts
@@ -1,6 +1,8 @@
-import { test, expect, beforeAll, afterAll, describe } from "vitest";
+import { it, expect, beforeAll, afterAll, describe } from "vitest";
 import { app } from "../app";
 import request from "supertest";
+import { beforeEach } from "node:test";
+import { execSync } from "node:child_process";
 
 describe("Transactions Routes", () => {
   beforeAll(async () => {
@@ -11,7 +13,12 @@ describe("Transactions Routes", () => {
     await app.close();
   });
 
-  test("User Can Create A New Transaction", async () => {
+  beforeEach(async () => {
+    execSync("npm run knex -- migrate:rollback --all");
+    execSync("npm run knex -- migrate:latest");
+  });
+
+  it("should create a new transaction", async () => {
     const response = await request(app.server).post("/transactions").send({
       title: "New Transaction",
       amount: 5000,
@@ -19,5 +26,102 @@ describe("Transactions Routes", () => {
     });
 
     expect(response.statusCode).toBe(201);
+  });
+
+  it("should list all transactions", async () => {
+    const createTransactionsResponse = await request(app.server)
+      .post("/transactions")
+      .send({
+        title: "New Transaction",
+        amount: 5000,
+        type: "credit",
+      });
+
+    const cookies = createTransactionsResponse.get("Set-Cookie");
+
+    if (!cookies) {
+      throw new Error("No cookies found in response");
+    }
+
+    const list = await request(app.server)
+      .get("/transactions")
+      .set("Cookie", cookies)
+      .expect(200);
+
+    expect(list.body.transactions).toEqual([
+      expect.objectContaining({
+        title: "New Transaction",
+        amount: 5000,
+      }),
+    ]);
+  });
+
+  it("should list a specific transaction", async () => {
+    const createTransactionsResponse = await request(app.server)
+      .post("/transactions")
+      .send({
+        title: "New Transaction",
+        amount: 5000,
+        type: "credit",
+      });
+
+    const cookies = createTransactionsResponse.get("Set-Cookie");
+
+    if (!cookies) {
+      throw new Error("No cookies found in response");
+    }
+
+    const list = await request(app.server)
+      .get("/transactions")
+      .set("Cookie", cookies)
+      .expect(200);
+
+    const transactionId = list.body.transactions[0].id;
+
+    const getTransactionResponse = await request(app.server)
+      .get(`/transactions/${transactionId}`)
+      .set("Cookie", cookies)
+      .expect(200);
+
+    expect(getTransactionResponse.body.transactions).toEqual(
+      expect.objectContaining({
+        title: "New Transaction",
+        amount: 5000,
+      })
+    );
+  });
+
+  it("should get a summary", async () => {
+    const createTransactionsResponse = await request(app.server)
+      .post("/transactions")
+      .send({
+        title: "New Transaction",
+        amount: 5000,
+        type: "credit",
+      });
+
+    const cookies = createTransactionsResponse.get("Set-Cookie");
+
+    if (!cookies) {
+      throw new Error("No cookies found in response");
+    }
+
+    await request(app.server)
+      .post("/transactions")
+      .set("Cookie", cookies)
+      .send({
+        title: "debit Transaction",
+        amount: 2000,
+        type: "debit",
+      });
+
+    const summaryResponse = await request(app.server)
+      .get("/transactions/summary")
+      .set("Cookie", cookies)
+      
+
+    expect(summaryResponse.body).toEqual({
+      amount: "3000",
+    });
   });
 });
